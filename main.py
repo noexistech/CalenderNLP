d9420297a549ed739329cf623fdb0186eb757de9
noexistech <dinhgiahuy.sgu@gmail.com>
2025-12-06 11:57:44 +0000

    Test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from nlp_engine import process_text
 
 #text = "Nhắc tôi chiều ngày kia đi tập thể dục ở công viên Hoàng Văn Thụ lúc 5h, nhắc trước 30 phút"
-text = "Nhắc tôi sáng ngày 2 tháng 1 năm 2026 đi họp lớp ở phòng c103 lúc 8h , nhắc trước 50 phút"
+#text = "Nhắc tôi sáng ngày 2 tháng 1 năm 2026 đi họp lớp ở phòng c103 lúc 8h , nhắc trước 50 phút"
 #text = "Nhắc tôi sáng mai dậy tập thể dục lúc 7 giờ 15 ở công viên Hoàng Văn Thụ"
-#text = "Nhắc tôi họp nhóm lúc 10 giờ sáng mai ở phòng 302, nhắc trước 15 phút"
+text = "Nhắc tôi họp nhóm lúc 10 giờ sáng mai ở phòng 302, nhắc trước 15 phút"
 #text = "Nhắc tôi họp lớp lúc 9 giờ ở phòng 302"
 print(process_text(text))
